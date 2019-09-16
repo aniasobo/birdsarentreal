9e5386e8d4110feb7780079954850e550af90269
Anna Sobolewska <sobolewska.a.m@gmail.com>
2019-09-16 14:20:48 +0100

    Popup toggle funcs

--- popup.js
+++ popup.js
@@ -1,6 +1,18 @@
 let changeColor = document.getElementById('changeColor');
+let toggleButton = document.getElementById('toggle');
+let toggleClasses = toggleButton.classList;
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
+let toggleIsOn = function() {
+  toggleClasses.add('toggle-on');
+  toggleClasses.remove('toggle-off');
+  toggleButton.innerHTML = 'Show me the lies';
+};
+
+let toggleIsOff = function() {
+  toggleClasses.add('toggle-off');
+  toggleClasses.remove('toggle-on');
+  toggleButton.innerHTML = 'Show me the truths';
+};
+
+// pseudocode
+
